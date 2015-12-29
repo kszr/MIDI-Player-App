3af1fbf468c8aba4f8eb25072087cdcc9a36a35c
achatterjee7 <abhishek.rob@gmail.com>
2015-12-29 11:00:29 -0800

    Made program list searchable, but not in a pretty way.

--- app/src/main/java/com/kszr/midiplayer/ProgramChangeActivity.java
+++ app/src/main/java/com/kszr/midiplayer/ProgramChangeActivity.java
@@ -3,10 +3,13 @@ package com.kszr.midiplayer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.kszr.midiplayer.constant.ProgramList;
@@ -20,12 +23,39 @@ import java.util.List;
  * Created by abhishekchatterjee on 12/22/15.
  */
 public class ProgramChangeActivity extends AppCompatActivity {
+    private EditText inputSearch;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_list);
         setUpProgramList();
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+        addInputTextChangedListener();
+    }
+
+    private void addInputTextChangedListener() {
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                arrayAdapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 
     /**
@@ -34,7 +64,7 @@ public class ProgramChangeActivity extends AppCompatActivity {
     private void setUpProgramList() {
         List<String> programList = getProgramList();
         final ListView listView = (ListView) findViewById(R.id.program_list);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, programList);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, programList);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
                 String instrument = (String) (listView.getItemAtPosition(myItemInt));
